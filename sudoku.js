cabe22eea4012ece2c6f09cf375f90ae23bd3ee3
Lace Hilaj <86404129+Adrion10@users.noreply.github.com>
2022-12-24 00:24:09 +0100

    Soduku2

--- sudoku.js
+++ sudoku.js
@@ -1,84 +1,86 @@
-var numSelected = null;
-var tileSelected = null;
+// var numSelected = null;
+// var tileSelected = null;
 
-var errors = 0;
+// var errors = 0;
 
-var board = [
-  "--74916-5",
-  "2---6-3-9",
-  "-----7-1-",
-  "-586----4",
-  "--3----9-",
-  "--62--187",
-  "9-4-7---2",
-  "67-83----",
-  "81--45---",
-];
-var solution = [
-  "387491625",
-  "241568379",
-  "569327418",
-  "758619234",
-  "123784569",
-  "496253187",
-  "934176852",
-  "675832941",
-  "812945763",
-];
-window.onload = function () {
-  setGame();
-};
-function setGame() {
-  for (let i = 1; i <= 9; i++) {
-    let number = document.createElement("div");
-    number.id = i;
-    number.innerText = i;
-    number.addEventListener("click", selectNumber);
-    number.classList.add("number");
-    document.getElementById("digits").appendChild(number);
-  }
-  for (let j = 0; j < 9; j++) {
-    for (let b = 0; b < 9; b++) {
-      let tile = document.createElement("div");
-      tile.id = j.toString() + "-" + b.toString();
-      if (board[j][b] != "-") {
-        tile.innerText = board[j][b];
-        tile.classList.add("tile-start");
-      }
-      if (j == 2 || j == 5) {
-        tile.classList.add("horizontal-line");
-      }
-      if (b == 2 || b == 5) {
-        tile.classList.add("vertical-line");
-      }
-      tile.addEventListener("click", selectTile);
-      tile.classList.add("tile");
-      document.getElementById("board").append(tile);
-    }
-  }
-}
-function selectNumber() {
-  if (numSelected != null) {
-    numSelected.classList.remove("number-selected");
-  }
-  numSelected = this;
-  numSelected.classList.add("number-selected");
-}
-function selectTile() {
-  if (numSelected) {
-    if (this.innerText != "") {
-      return;
-    }
+// var board = [
+//   "--74916-5",
+//   "2---6-3-9",
+//   "-----7-1-",
+//   "-586----4",
+//   "--3----9-",
+//   "--62--187",
+//   "9-4-7---2",
+//   "67-83----",
+//   "81--45---",
+// ];
+// var solution = [
+//   "387491625",
+//   "241568379",
+//   "569327418",
+//   "758619234",
+//   "123784569",
+//   "496253187",
+//   "934176852",
+//   "675832941",
+//   "812945763",
+// ];
+// window.onload = function () {
+//   setGame();
+// };
+// function setGame() {
+//   for (let i = 1; i <= 9; i++) {
+//     let number = document.createElement("div");
+//     number.id = i;
+//     number.innerText = i;
+//     number.addEventListener("click", selectNumber);
+//     number.classList.add("number");
+//     document.getElementById("digits").appendChild(number);
+//   }
+//   for (let j = 0; j < 9; j++) {
+//     for (let b = 0; b < 9; b++) {
+//       let tile = document.createElement("div");
+//       tile.id = j.toString() + "-" + b.toString();
+//       if (board[j][b] != "-") {
+//         tile.innerText = board[j][b];
+//         tile.classList.add("tile-start");
+//       }
+//       if (j == 2 || j == 5) {
+//         tile.classList.add("horizontal-line");
+//       }
+//       if (b == 2 || b == 5) {
+//         tile.classList.add("vertical-line");
+//       }
+//       tile.addEventListener("click", selectTile);
+//       tile.classList.add("tile");
+//       document.getElementById("board").append(tile);
+//     }
+//   }
+// }
+// function selectNumber() {
+//   if (numSelected != null) {
+//     numSelected.classList.remove("number-selected");
+//   }
+//   numSelected = this;
+//   numSelected.classList.add("number-selected");
+// }
+// function selectTile() {
+//   if (numSelected) {
+//     if (this.innerText != "") {
+//       return;
+//     }
 
-    let coords = this.id.split("-");
-    let j = parseInt(coords[0]);
-    let b = parseInt(coords[1]);
+//     let coords = this.id.split("-");
+//     let j = parseInt(coords[0]);
+//     let b = parseInt(coords[1]);
 
-    if (solution[j][b] == numSelected.id) {
-      this.innerText = numSelected.id;
-    } else {
-      errors += 1;
-      document.getElementById("errors").innerText = errors;
-    }
-  }
-}
+//     if (solution[j][b] == numSelected.id) {
+//       this.innerText = numSelected.id;
+//     } else {
+//       errors += 1;
+//       document.getElementById("errors").innerText = errors;
+//     }
+//   }
+// }
+
+// Sudoku2
