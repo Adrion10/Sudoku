8ad1cf6c6e5c2ee884f32801fe1b0be2ec681b43
Lace Hilaj <86404129+Adrion10@users.noreply.github.com>
2023-01-02 21:29:49 +0100

    function timeConversion(time)

--- sudoku.js
+++ sudoku.js
@@ -147,9 +147,13 @@ function startTimer() {
     id("timer").textContent = timeConversion(timeRemaining);
   }, 1000);
 }
-
+// Converts seconds into string of MM:SS format
 function timeConversion(time) {
-  //Converts seconds to minutes
+  let minutes = Math.floor(time / 60);
+  if (minutes < 10) minutes = "0" + minutes;
+  let seconds = time % 60;
+  if (seconds < 10) seconds = "0" + seconds;
+}
 
 function generateBoard(board) {
   // clear previous board
